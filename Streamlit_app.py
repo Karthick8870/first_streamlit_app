302c7bf5d08c0e019fa8c1404eb726b654a7bca1
Karthick8870 <125533571+Karthick8870@users.noreply.github.com>
2023-02-16 16:28:35 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('* Dosa')
-streamlit.text('* Masal Dosa')
-streamlit.text('* Idli')
-streamlit.text('* Poori')
-streamlit.text('* Pongal')
+streamlit.text('🥪 Dosa')
+streamlit.text('🥫 Masal Dosa')
+streamlit.text('🥗 Idli')
+streamlit.text('🥙 Poori')
+streamlit.text('🌮 Pongal')
